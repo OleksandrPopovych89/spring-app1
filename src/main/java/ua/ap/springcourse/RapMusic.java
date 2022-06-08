595974b3b6cc0fd100416d560f8a4a2d1feb137a
Oleksandr <oleksandrpopovych89@gmail.com>
2022-06-09 01:18:16 +0300

    completed lesson 10

--- src/main/java/ua/ap/springcourse/RapMusic.java
+++ src/main/java/ua/ap/springcourse/RapMusic.java
@@ -1,6 +1,8 @@
 package ua.ap.springcourse;
 
-public class RapMusic implements Music{
+import org.springframework.stereotype.Component;
+//@Component
+public class RapMusic implements Music {
     @Override
     public String getSong() {
         return "Venom";
--- src/main/java/ua/ap/springcourse/TestSpring.java
+++ src/main/java/ua/ap/springcourse/TestSpring.java
@@ -8,16 +8,23 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music1 = context.getBean("rockMusic", RockMusic.class);
-        System.out.println(music1.getSong());
+//        Music music1 = context.getBean("rockMusic", RockMusic.class);
+//        System.out.println(music1.getSong());
+//
+//        MusicPlayer player1 = new MusicPlayer(music1);
+//        player1.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", ClassicalMusic.class);
+//
+//        MusicPlayer player2 = new MusicPlayer(music2);
+//        player2.playMusic();
+//
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        musicPlayer.playMusic();
 
-        MusicPlayer player1 = new MusicPlayer(music1);
-        player1.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", ClassicalMusic.class);
-
-        MusicPlayer player2 = new MusicPlayer(music2);
-        player2.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
